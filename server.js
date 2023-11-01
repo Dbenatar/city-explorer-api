e3a636b8b2aeb094dfc436402602d0ff1d4af900
Dom <dom.benatar@hotmail.com>
2023-11-01 15:52:31 +0000

    commit

--- server.js
+++ server.js
@@ -8,14 +8,11 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.get("/", (_, response) => response.json("Root route."));
 const data = require("./data/weather.json");
 
-const data =require("./data/weather.json");
-
 app.get("/weather", (request, response) => {
-  request.query.searchQuery
   const lat = request.query.lat;
   const lon = request.query.lon;
   const searchQuery = request.query.searchQuery;
@@ -25,14 +22,13 @@ app.get("/weather", (request, response) => {
       city.city_name === searchQuery && city.lat === lat && city.lon === lon
     );
   });
-    const wrangledData = filteredCity.data.map((day) => {
-      return {
-        description: day.weather.description,
-        date: day.datetime,
+  const wrangledData = filteredCity.data.map((day) => {
+    return {
+      description: day.weather.description,
+      date: day.datetime,
     };
-    });
-    response.json(wrangledData);
-
+  });
+  response.json(wrangledData);
 });
 
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
