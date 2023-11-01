168bf913ebb3fc18109afe87bae855ff4643b7ad
Dom <dom.benatar@hotmail.com>
2023-11-01 14:07:37 +0000

    lat lon search added

--- server.js
+++ server.js
@@ -8,15 +8,31 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(cors())
+app.get("/", (_, response) => response.json("Root route."));
+const data = require("./data/weather.json");
+
 const data =require("./data/weather.json");
 
-function findCityByWeather () {
-  return data.find(())
-}
-
 app.get("/weather", (request, response) => {
-  const weather = cityWeather(request.query.lat.lon)
-  response.json(weather);
+  request.query.searchQuery
+  const lat = request.query.lat;
+  const lon = request.query.lon;
+  const searchQuery = request.query.searchQuery;
+
+  const filteredCity = data.find((city) => {
+    return (
+      city.city_name === searchQuery && city.lat === lat && city.lon === lon
+    );
+  });
+    const wrangledData = filteredCity.data.map((day) => {
+      return {
+        description: day.weather.description,
+        date: day.datetime,
+    };
+    });
+    response.json(wrangledData);
+
 });
 
-
+app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
